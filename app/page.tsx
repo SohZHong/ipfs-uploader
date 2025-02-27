fd927cdfa28d6cc1ae452c4dffdd835700434355
SohZHong <sohzhehong09@gmail.com>
2025-02-27 21:08:16 +0800

    added retrieval from public IPFS node

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FileUpload } from '@/components/ui/file-upload';
 import axios from 'axios';
 
 export default function Home() {
   const [files, setFiles] = useState<File[]>([]);
+  const [ipfs, setIpfs] = useState<string>('');
+  const [uploadedFileSrc, setUploadedFileSrc] = useState<string>();
+
   const handleFileUpload = (files: File[]) => {
     setFiles(files);
-    console.log(files);
   };
 
   const uploadFile = async (file: File) => {
@@ -16,17 +18,18 @@ export default function Home() {
       const formData = new FormData();
       formData.append('file', file);
 
-      // Send FormData
-      const response = await axios.post('/api/upload', formData, {
+      const response = await axios.post('/api/pinata/upload', formData, {
         headers: { 'Content-Type': 'multipart/form-data' },
       });
 
-      console.log(response.data);
+      setIpfs(response.data.IpfsHash);
+      setFiles([]);
       console.log('File uploaded successfully!');
     } catch (error) {
       console.error('File upload failed:', error);
     }
   };
+
   const handleButtonClick = async () => {
     if (files.length === 0) {
       console.warn('No files selected');
@@ -36,15 +39,25 @@ export default function Home() {
     await uploadFile(files[0]);
   };
 
+  useEffect(() => {
+    if (ipfs) {
+      setUploadedFileSrc(`https://gateway.pinata.cloud/ipfs/${ipfs}`);
+    }
+  }, [ipfs]);
+
   return (
     <div className='grid grid-rows-[auto_1fr_auto] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20'>
       <div className='flex-col justify-center flex items-center gap-2'>
         <h1 className='text-2xl font-black'>IPFS Image Uploader</h1>
-        <p className='brightness-50'>
-          Upload your files to IPFS and receive its CID
+        <p>
+          <span className='brightness-50'>
+            Upload your files to IPFS and receive its CID{' '}
+          </span>
+          <br></br>
+          <span className='brightness'>* Please reload after an upload</span>
         </p>
       </div>
-      <main className='flex flex-col gap-8 row-start-2 items-center sm:items-start'>
+      <main className='flex flex-col gap-8 row-start-2 items-center'>
         <div className='w-full max-w-4xl mx-auto min-h-96 border border-dashed bg-white dark:bg-black border-neutral-200 dark:border-neutral-800 rounded-lg'>
           <FileUpload onChange={handleFileUpload} />
         </div>
@@ -54,6 +67,16 @@ export default function Home() {
         >
           Upload File
         </button>
+        {uploadedFileSrc && (
+          <div className='flex flex-col items-center gap-2'>
+            <img
+              src={uploadedFileSrc}
+              alt='Uploaded IPFS File'
+              className='lg:max-w-1/2 rounded-lg shadow-lg'
+            />
+            <p className='text-sm break-all'>CID: {ipfs}</p>
+          </div>
+        )}
       </main>
     </div>
   );
--- common/environment.ts
+++ common/environment.ts
@@ -1,7 +1,8 @@
 import { PinataSDK } from 'pinata-web3';
 
 const PINATA_JWT = String(process.env.PINATA_JWT || '');
-export const PINATA_GATEWAY = String(process.env.PINATA_GATEWAY || '');
+const PINATA_GATEWAY = String(process.env.PINATA_GATEWAY || '');
+
 // Validate that all required environment variables are set
 if (!PINATA_JWT || !PINATA_GATEWAY) {
   throw new Error(
